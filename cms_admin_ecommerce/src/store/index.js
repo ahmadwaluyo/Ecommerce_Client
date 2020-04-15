2c072e3712656387271d032f36046b415b6afd0e
ahmadwaluyo <ahmad.umb15@gmail.com>
2020-04-15 15:48:40 +0700

    change baseUrl to debugging

--- cms_admin_ecommerce/src/store/index.js
+++ cms_admin_ecommerce/src/store/index.js
@@ -3,8 +3,8 @@ import Vuex from 'vuex'
 import axios from 'axios'
 
 Vue.use(Vuex)
-// const baseURL = 'https://ecommercehacktiv8.herokuapp.com'
-const baseURL = 'http://localhost:3000'
+const baseURL = 'https://ecommercehacktiv8.herokuapp.com'
+// const baseURL = 'http://localhost:3000'
 
 export default new Vuex.Store({
   state: {
